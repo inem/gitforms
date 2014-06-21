5f09b363ec011135c5fdec97890f32a2416b25d8
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-06-21 22:36:22 +0700

    added tomdoc documentation

--- lib/stupid_storeman.rb
+++ lib/stupid_storeman.rb
@@ -1,16 +1,28 @@
 class StupidStoreman
-  attr_accessor :dir
+  attr_reader :dir
   def initialize(dir)
     @dir = dir
   end
 
+  # Public: Duplicate some text an arbitrary number of times.
+  #
+  # data - Anything, most probabaly Hash.
+  #
+  # Examples
+  #
+  #   store!(a: 2, b: 3)
+  #   # => true
+  #
+  # Returns true
+  # Raises RuntimeError if destination file alredy exists
   def store!(data)
     FileUtils.mkdir_p(dir)
-    raise RuntimeError if File.exist?(filepath)
+    raise RuntimeError.new "File '#{filepath}' already exists!" if File.exist?(filepath)
 
     File.open(filepath, 'w') do |file|
       file.write MultiJson.dump(data)
     end
+    true
   end
 
   def filepath
