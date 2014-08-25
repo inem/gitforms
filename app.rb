dd3ca6c4eeebef03ed565aa64d1f3a058606af50
Ivan Nemytchenko <nemytchenko@gmail.com>
2014-08-25 21:24:20 +0700

    view

--- app.rb
+++ app.rb
@@ -1,33 +1,22 @@
 require_relative 'gitforms'
 require_relative 'fork_and_send_pull_request'
 
-require 'forme'
 require 'sinatra'
 require 'dotenv'
 Dotenv.load
 
-get '/' do
-  @aula = Aula.new
-  a = Forme.form(@aula, :method => 'POST') do |f|
-    f.input :title, :placeholder => :Title
-    f.input :year, :placeholder => :Year
-    f.input :url, :placeholder => :URL
-    f.input :imageUrl, :placeholder => 'Image URL'
-    f.input :description, :placeholder => 'Description'
-    f.input :tags, :placeholder => 'tag1, tag2, tag3, ..'
-    f.input :speakers, :placeholder => 'speaker1, speaker2'
-    f.input :location, :placeholder => "Location"
-    f.button
-  end
-  a.to_s
+get '/aula.io' do
+  haml :aula
 end
 
-post '/' do
+post '/aula.io' do
   aula = Aula.new(params)
   github = Gitforms::Github.new(system_login, password)
 
   use_case = ForkAndSendPullRequest.new(aula, github)
+  # use_case.run("hnrc", "aula")
   use_case.run("mkaschenko", "mkaschenko.github.io")
+  redirect '/aula.io'
 end
 
 private
